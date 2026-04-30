f87089fc03cfc43492af2c5a365b6eb3307a56cb
ShirleyChung <jonesr_tw@yahoo.com.tw>
2026-04-30 21:50:09 +0800

    fix import

--- src/llm/toolDispatcher.js
+++ src/llm/toolDispatcher.js
@@ -7,7 +7,6 @@
 import { createReminder } from "../services/reminderService.js";
 import { buildSessionKey } from "../services/conversationStateService.js";
 import { getTodayLinkFromSheet } from "../services/sheetLinkService.js";
-import { getTodayEvents, createCalendarEvent } from "../services/calendarService.js";
 import { replyText } from "../line/reply.js";
 import { lineClient } from "../line/client.js";
 
